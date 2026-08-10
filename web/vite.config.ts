900cf8085897d1af6bbc25dc776ddf68b9a67c9e
Leslie-23 <leslieajayi27@gmail.com>
2026-08-10 11:03:04 +0000

    fixed vite issues

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,13 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'node:path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "node:path";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tsconfigPaths({
+      projects: ["tsconfig.app.json", "tsconfig.node.json"],
+    }),
+  ],
   resolve: {
     alias: {
-      '@domain': path.resolve(__dirname, '../src/domain'),
+      "@domain": path.resolve(__dirname, "../src/domain"),
     },
   },
-})
+});
